4be10c6ddbd756d0cb9b6cd3c4b3eade75f07120
Mar Alonso <mar.alonso@udc.es>
2022-04-30 23:23:39 +0200

    info.js added

---
+++ static/js/info.js
@@ -0,0 +1,14 @@
+const form = document.getElementById("form");
+const inputFile = document.getElementById("file");
+
+const formData = new FormData();
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+
+    for (const file of inputFile.files) {
+        formData.append("files", file);
+    }
+};
+
+form.addEventListener("submit", handleSubmit);
