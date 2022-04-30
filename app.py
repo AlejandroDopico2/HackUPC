7c2ff27b4680c8e6bb9067cea87a76967e6735d1
AlejandroDopico2 <alejandro.dopico2@udc.es>
2022-04-30 23:12:27 +0200

    changes done in flask

--- app.py
+++ app.py
@@ -23,39 +23,40 @@ app.model = ScoreImage() #Aquí ponemos una instancia de nuestro modelo
 app.decisionTree= pickle.load(open('model.pkl', 'rb'))
 
 
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route("/results", methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
-
-        kitchen = request.form['kitchen']
+        kitchen = request.form['meters']
         bathroom = request.form['bathroom']
-        frontal = request.form['frontal']
-        bedroom = request.form['bedroom']
+        frontal = request.form['zip']
+        bedroom = request.form['bedrooms']
 
 
         #Ejecutamos nuestro modelo para ver si se pasan todas las fotos
         try:
-                data = app.model.getScoreImages(kitchen, bathroom, frontal, bedroom)
-                output = model.predict(data)
-                return redirect(url_for('results'), score = format(output)) #Si va fino
+            print("Aqui entra")
+            data = app.model.getScoreImages(kitchen, bathroom, frontal, bedroom)
+            output = app.decisionTree.predict(data)
+            return redirect(url_for('results'), score = format(output)) #Si va fino
         except Exception as e:
             print(e)
             flash('Error al conectarse a la base de datos.')
-    return redirect(url_for('upload')) #Si no hacemos algo bien 
+    return redirect(url_for('results')) #Si no hacemos algo bien 
 
-@app.route('/results')
+@app.route('/info')
 def results():
-    return render_template('results.html')
+    return render_template('info.html')
 
 @app.route("/")
 def root():
     return render_template('index.html')
+    # return redirect(url_for('index.html'))
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
         'favicon.ico',mimetype='image/vnd.microsoft.icon')
 
-@app.route('/about_us')
+@app.route("/about_us")
 def aboutUs():
     return render_template('about_us.html')
